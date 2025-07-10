f511841d7b6d2b321886b3118be5d1943954a787
LE Weihua <weihua.le@origi-tech.com>
2025-07-10 10:30:13 +0800

    添加自动化部署

--- MainFrom/obj/Debug/net8.0-windows/MainFrom.AssemblyInfo.cs
+++ MainFrom/obj/Debug/net8.0-windows/MainFrom.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MainFrom")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a35534a2e89e865b1ccc7bd4439c482afa48210f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff114b0262e608eb140959efda923a0a65597b0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MainFrom")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MainFrom")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

27603e45c9db17fbded0bf563818dc8bab44c9be
LE Weihua <weihua.le@origi-tech.com>
2025-07-16 10:31:07 +0800

    update

--- UserData/obj/Debug/net8.0-windows/UserData.AssemblyInfo.cs
+++ UserData/obj/Debug/net8.0-windows/UserData.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserData")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+686f2bb0114c35ed661c55cb52c9f6bc05be81de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c7506bf21d4882cb549460e3cbe7355ecdc630a")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserData")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserData")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ViewModel/obj/Debug/net8.0-windows/ViewModel.AssemblyInfo.cs
+++ ViewModel/obj/Debug/net8.0-windows/ViewModel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ViewModel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+686f2bb0114c35ed661c55cb52c9f6bc05be81de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c7506bf21d4882cb549460e3cbe7355ecdc630a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ViewModel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ViewModel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
